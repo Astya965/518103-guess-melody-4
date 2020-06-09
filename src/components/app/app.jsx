892075a6ea35ad75ffb49bf5fcd581651a06bcf1
astya965 <astya965@gmail.com>
2020-06-09 19:12:58 +0300

    Feat: Add eslint-disable-next-line for props error

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
 const App = (props) => {
+  // eslint-disable-next-line react/prop-types
   const {errorCount} = props;
 
   return <WelcomeScreen errorCount={errorCount} />;
