91c440c37a94a2963e47a53dd721daf6583d5307
astya965 <astya965@gmail.com>
2020-06-18 17:50:10 +0300

    feat(3-1): add questions props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,22 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import QuestionArtist from "../question-artist/question-artist.jsx";
+import QuestionGenre from "../question-genre/question-genre.jsx";
+
 
 const welcomeButtonHandler = () => {};
 
 const App = (props) => {
-  const {errorCount} = props;
+  const {errorCount, questions} = props;
 
-  return <WelcomeScreen
-    errorCount={errorCount}
-    onWelcomeButtonClick={welcomeButtonHandler}
-  />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorCount={errorCount}
+            onWelcomeButtonClick={welcomeButtonHandler}/>
+        </Route>
+        <Route exact path="/dev-artist">
+          <QuestionArtist question={questions[1]}/>
+        </Route>
+        <Route exact path="/dev-genre">
+          <QuestionGenre question={questions[0]}/>
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 App.propTypes = {
   errorCount: PropTypes.number.isRequired,
-  questions: PropTypes.array.isRequired,
+  questions: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
 export default App;
--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -4,11 +4,46 @@ import renderer from "react-test-renderer";
 import App from "./app.jsx";
 
 const testErrorCount = 2;
+const testQuestions = [
+  {
+    type: `genre`,
+    genre: `folk`,
+    answers: [{
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `rock`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `blues`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `jazz`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `folk`,
+    }],
+  }, {
+    type: `artist`,
+    song: {
+      artist: `Aaron Dunn`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/f/fb/Aaron_Dunn_-_Sonata_No_1_-_Movement_1.ogg`,
+    },
+    answers: [{
+      picture: `https://api.adorable.io/avatars/122`,
+      artist: `Aaron Dunn`,
+    }, {
+      picture: `https://api.adorable.io/avatars/128`,
+      artist: `Daniel Veesey`,
+    }, {
+      picture: `https://api.adorable.io/avatars/54`,
+      artist: `Karine Gilanyan`,
+    }],
+  }
+];
 
 it(`Render App`, () => {
   const tree = renderer.
     create(
-        <App errorCount={testErrorCount} />
+        <App errorCount={testErrorCount} questions={testQuestions}/>
     ).toJSON();
 
   expect(tree).toMatchSnapshot();
