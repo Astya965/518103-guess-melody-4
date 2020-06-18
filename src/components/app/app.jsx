965853c2aad65da2137b3c423d8fa55c45680d62
astya965 <astya965@gmail.com>
2020-06-18 20:57:13 +0300

    feat(3-1): add Router to App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,30 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 import QuestionArtist from "../question-artist/question-artist.jsx";
 import QuestionGenre from "../question-genre/question-genre.jsx";
-
-
-const welcomeButtonHandler = () => {};
+import {GameType} from "../../utils/const.js";
 
 const App = (props) => {
   const {errorCount, questions} = props;
+  const [step, setStep] = useState(-1);
+  const question = questions[step];
+
+  const renderScreen = () => {
+    if (step === -1 || step >= questions.length) {
+      return (
+        <WelcomeScreen
+            errorCount={errorCount}
+            onWelcomeButtonClick={() => setStep(0)}/>
+      )
+    }
+
+    if (question) {
+      switch (question.type) {
+        case GameType.ARTIST:
+          return (
+            <QuestionArtist question={questions[1]}
+              onAnswer={() => setStep(step + 1)}/>
+          );
+        case GameType.GENRE:
+            return (
+              <QuestionGenre question={questions[0]}
+                onAnswer={() => setStep(step + 1)}/>
+            );
+      }
+    }
+
+    return null;
+  }
 
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <WelcomeScreen
-            errorCount={errorCount}
-            onWelcomeButtonClick={welcomeButtonHandler}/>
+          {renderScreen()}
         </Route>
         <Route exact path="/dev-artist">
-          <QuestionArtist question={questions[1]}/>
+          <QuestionArtist question={questions[1]}
+            onAnswer={() => {}}/>
         </Route>
         <Route exact path="/dev-genre">
-          <QuestionGenre question={questions[0]}/>
+          <QuestionGenre question={questions[0]}
+            onAnswer={() => {}}/>
         </Route>
       </Switch>
     </Router>
