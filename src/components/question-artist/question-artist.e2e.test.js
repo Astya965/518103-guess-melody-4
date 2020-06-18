fc0e5ef922c11017e4148840e5cf63bba282c55b
astya965 <astya965@gmail.com>
2020-06-18 23:50:17 +0300

    test(3-1): add e2e-test for QuestionArtist

---
+++ src/components/question-artist/question-artist.e2e.test.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {shallow} from "enzyme";
+
+import QuestionArtist from "./question-artist.jsx";
+
+const mock = {
+  question: {
+    type: `artist`,
+    song: {
+      artist: `one`,
+      src: ``
+    },
+    answers: [
+      {
+        artist: `one`,
+        picture: `pic-one`,
+      },
+      {
+        artist: `two`,
+        picture: `pic-two`,
+      },
+      {
+        artist: `three`,
+        picture: `pic-three`,
+      },
+    ],
+  }
+};
+
+describe(`QuestionArtist tests`, () => {
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it(`Click on user answer should pass to the callback value of target input`, () => {
+    const {question} = mock;
+    const onAnswer = jest.fn();
+    const userAnswer = `one`;
+
+    const screen = shallow(<QuestionArtist
+      onAnswer={onAnswer}
+      question={question}
+    />);
+
+    const answerInputs = screen.find(`input`);
+    const answerOne = answerInputs.at(0);
+
+    answerOne.simulate(`change`, {target: {value: `one`}});
+
+    expect(onAnswer).toHaveBeenCalledTimes(1);
+    expect(onAnswer).toHaveBeenCalledWith(userAnswer, question.song.artist);
+  });
+});
