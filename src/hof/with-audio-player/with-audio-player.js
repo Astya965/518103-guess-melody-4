ed1d89f1e5c28c1728a2869c0df737ad9033aa0d
astya965 <astya965@gmail.com>
2020-06-25 01:40:13 +0300

    feat(4-1): add the withAudioPlayer higher-order function

---
+++ src/hof/with-audio-player/with-audio-player.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+import AudioPlayer from "../../components/audio-player/audio-player.jsx";
+
+const withActivePlayer = (Component) => (props) => {
+  const [activePlayer, setActivePlayer] = useState(0);
+
+  return <Component
+    {...props}
+    renderPlayer={(src, id) => {
+      const handleActiveChange = () => setActivePlayer(activePlayer === id ? -1 : id);
+
+      return <AudioPlayer
+        src={src}
+        isStarting={id === activePlayer}
+        onPlayButtonClick={handleActiveChange}
+      />;
+    }}
+  />;
+};
+
+export default withActivePlayer;
