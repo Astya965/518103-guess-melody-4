472ce2d43642bf0787bf95ad5e58819aeb443f9e
astya965 <astya965@gmail.com>
2020-07-04 19:36:39 +0000

    feat(5-1): add game-header to components

--- src/components/question-genre/question-genre.jsx
+++ src/components/question-genre/question-genre.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import PropTypes from "prop-types";
 
+import GameHeader from "../../components/game-header/game-header.jsx";
 import AudioPlayer from "../../components/audio-player/audio-player.jsx";
 
 const QuestionGenre = (props) => {
@@ -26,23 +27,7 @@ const QuestionGenre = (props) => {
 
   return (
     <section className="game game--genre">
-      <header className="game__header">
-        <a className="game__back" href="#">
-          <span className="visually-hidden">Сыграть ещё раз</span>
-          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
-        </a>
-
-        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-          <circle className="timer__line" cx="390" cy="390" r="370"
-            style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
-        </svg>
-
-        <div className="game__mistakes">
-          <div className="wrong"></div>
-          <div className="wrong"></div>
-          <div className="wrong"></div>
-        </div>
-      </header>
+      <GameHeader />
 
       <section className="game__screen">
         <h2 className="game__title">Выберите {genre} треки</h2>
