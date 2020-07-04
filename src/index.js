e464746e98631f13d09544252907bc73a601ff03
astya965 <astya965@gmail.com>
2020-07-04 16:40:02 +0000

    feat(5-1): add Provider and store to index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducer} from "./reducers/reducer.js";
 
 import App from "./components/app/app.jsx";
-import questions from "./mocks/questions.js";
 
-const settings = {
-  errorCount: 3,
-};
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDOM.render(
-    <App errorCount={settings.errorCount} questions={questions} />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
     document.querySelector(`#root`)
 );
