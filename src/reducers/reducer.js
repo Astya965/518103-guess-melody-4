60dcc5261acc8f6c77c53f46744a2b0f0f0ed07a
astya965 <astya965@gmail.com>
2020-07-04 16:38:25 +0000

    feat(5-1): add ActionCreator

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,6 +1,51 @@
+import {extendObject} from "../utils/utils.js";
+import {GameType} from '../utils/const.js';
+import questions from "../mocks/questions.js";
+
 const initialState = {
   mistakes: 0,
   maxMistakes: 3,
   step: -1,
   questions,
-}
+};
+
+export const ActionType = {
+  INCREMENT_MISTAKES: `INCREMENT_MISTAKES`,
+  INCREMENT_STEP: `INCREMENT_STEP`,
+};
+
+const isArtistAnswerCorrect = (question, userAnswer) => {
+  return userAnswer === question.song.artist;
+};
+
+const isGenreAnswerCorrect = (question, userAnswer) => {
+  return Object.entries(userAnswer)
+    .every(([key, value]) => {
+      return value === (question.answers[key].genre === question.genre);
+    });
+};
+
+export const ActionCreator = {
+  incrementMistake: (question, userAnswer) => {
+    let answerIsCorrect = false;
+
+    switch (question.type) {
+      case GameType.ARTIST:
+        answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
+        break;
+      case GameType.GENRE:
+        answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
+        break;
+    }
+
+    return {
+      type: ActionType.INCREMENT_MISTAKES,
+      payload: answerIsCorrect ? 0 : 1,
+    };
+  },
+
+  incrementStep: () => ({
+    type: ActionType.INCREMENT_STEP,
+    payload: 1,
+  }),
+};
