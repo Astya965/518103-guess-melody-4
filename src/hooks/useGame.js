07542ea099d881b118d0bb7413cb2641da20ba9f
astya965 <astya965@gmail.com>
2020-07-15 14:53:53 +0000

    feat(5-1): add hook for game process

---
+++ src/hooks/useGame.js
@@ -0,0 +1,38 @@
+import {useEffect} from "react";
+import {useSelector, useDispatch} from "react-redux";
+import {ActionCreator} from "../store/reducer.js";
+import {getStep, getMistakes, getMaxMistakes, getQuestions} from "../store/selectors.js";
+
+import useAnswer from "../hooks/useAnswer.js";
+
+const useGame = () => {
+  const maxMistakes = useSelector(getMaxMistakes);
+  const mistakes = useSelector(getMistakes);
+  const questions = useSelector(getQuestions);
+  const step = useSelector(getStep);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (mistakes >= maxMistakes || step >= questions.length) {
+      dispatch(ActionCreator.resetGame());
+    }
+  });
+
+  const onWelcomeButtonClick = () => {
+    dispatch(ActionCreator.incrementStep());
+  };
+
+  const onAnswer = (question, answer) => {
+    useAnswer(question, answer, dispatch);
+  };
+
+  return {
+    maxMistakes,
+    questions,
+    step,
+    onWelcomeButtonClick,
+    onAnswer
+  };
+};
+
+export default useGame;
