b6556a869ab2ca97bc4210a50049eeec875abba5
astya965 <astya965@gmail.com>
2020-07-22 19:47:03 +0000

    feat(6-1): add logic of changing screens

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,21 +4,33 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 import QuestionArtist from "../question-artist/question-artist.jsx";
 import QuestionGenre from "../question-genre/question-genre.jsx";
+import Result from "../result/result.jsx";
+
 import {GameType} from "../../utils/const.js";
 import useGame from "../../hooks/useGame.js";
 
 const App = () => {
-  const {questions, step, maxMistakes, onWelcomeButtonClick, onAnswer} = useGame();
+  const {questions, step, mistakes, maxMistakes, onWelcomeButtonClick, onResetButtonClick, onAnswer} = useGame();
 
   const currentQuestion = questions[step];
 
   const renderScreen = () => {
-    if (step === -1 || step >= questions.length) {
+    if (step === -1) {
       return (
         <WelcomeScreen
           errorCount={maxMistakes}
           onWelcomeButtonClick={onWelcomeButtonClick}/>
       );
+    }
+
+    if (mistakes >= maxMistakes) {
+      return <Result isSuccess={false}
+        onResetButtonClick={onResetButtonClick}/>;
+    }
+
+    if (step >= questions.length) {
+      return <Result isSuccess={true} mistakes={mistakes} right={questions.length - mistakes}
+        onResetButtonClick={onResetButtonClick} />;
     }
 
     if (currentQuestion) {
