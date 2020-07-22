9c4f6c0819f6bca65dcd6d83fc952debee1b84f4
astya965 <astya965@gmail.com>
2020-07-22 20:37:53 +0000

    feat(5-1): move checkUserAnswer to useGame

--- src/hooks/useGame.js
+++ src/hooks/useGame.js
@@ -1,8 +1,19 @@
 import {useEffect} from "react";
 import {useSelector, useDispatch} from "react-redux";
 import {ActionCreator, getStep, getMistakes, getMaxMistakes, getQuestions} from "../store/reducer.js";
+import {isArtistAnswerCorrect, isGenreAnswerCorrect} from "../utils/utils.js";
+import {GameType} from "../utils/const.js";
 
-import useAnswer from "../hooks/useAnswer.js";
+const checkUserAnswer = (question, userAnswer) => {
+  switch (question.type) {
+    case GameType.ARTIST:
+      return isArtistAnswerCorrect(question, userAnswer);
+    case GameType.GENRE:
+      return isGenreAnswerCorrect(question, userAnswer);
+  }
+
+  return false;
+};
 
 const useGame = () => {
   const maxMistakes = useSelector(getMaxMistakes);
@@ -22,9 +33,12 @@ const useGame = () => {
   };
 
   const onAnswer = (question, answer) => {
-    useAnswer(question, answer).forEach((action) => {
-      dispatch(action);
-    });
+    if (checkUserAnswer(question, answer)) {
+      dispatch(ActionCreator.processCorrectAnswer());
+    } else {
+      dispatch(ActionCreator.processWrongAnswer());
+    }
+    dispatch(ActionCreator.incrementStep());
   };
 
   return {
